bb96cc20b41928c0dd5ea43faf46448fe178e736
JoshuaMiguel21 <joshuamiguel.embestro.cics@ust.edu.ph>
2024-02-02 18:17:37 +0800

    added owner item

--- pages/owner_item.php
+++ pages/owner_item.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/register.css">
+    <link rel="stylesheet" href="../css/customer_item.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
@@ -13,7 +13,40 @@
 </head>
 
 <body>
+    <nav>
+        <a href="customer_homepage.html"><i class="fas fa-home"></i></a>
+        <input type="text" id="search" placeholder="Search..">
+        <div class="right-side">
+            <a href="#" class="active">Shop</a>
+            <a href="#"><i class="fas fa-user"></i></a>
+            <a href="#"><i class="fas fa-shopping-cart"></i></a>
+        </div>
+    </nav>
+    <footer>
+        <div class="footer-info">
+            <div>
+                <p><img src="/res/logo.png" alt="Yeokart Logo" class="footer-logo">&copy; 2024 Yeokart. All rights reserved.</p>
+            </div>
 
+            <div class="footer-links">
+                <a href="#">Home</a>
+                <a href="customer_item.html">Shop</a>
+                <a href="#">Contact</a>
+                <a href="#">About Us</a>
+            </div>
+
+            <div class="social-links">
+                <li><a href="#" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+                <li><a href="#" target="_blank"><i class="fab fa-twitter"></i></a></li>
+                <li><a href="#" target="_blank"><i class="fab fa-instagram"></i></a></li>
+            </div>
+        </div>
+    </footer>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.0.7/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://kit.fontawesome.com/your-fontawesome-kit.js" crossorigin="anonymous"></script>
 </body>
 
 </html>
