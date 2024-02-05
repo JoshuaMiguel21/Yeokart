3b0eddb61c31cbaa7b42f68a541ee2fd9d6b8161
JoshuaMiguel21 <joshuamiguel.embestro.cics@ust.edu.ph>
2024-02-05 12:06:06 +0800

    .

--- pages/owner_item.php
+++ pages/owner_item.php
@@ -13,35 +13,7 @@
 </head>
 
 <body>
-    <nav>
-        <a href="customer_homepage.html"><i class="fas fa-home"></i></a>
-        <input type="text" id="search" placeholder="Search..">
-        <div class="right-side">
-            <a href="#" class="active">Shop</a>
-            <a href="#"><i class="fas fa-user"></i></a>
-            <a href="#"><i class="fas fa-shopping-cart"></i></a>
-        </div>
-    </nav>
-    <footer>
-        <div class="footer-info">
-            <div>
-                <p><img src="/res/logo.png" alt="Yeokart Logo" class="footer-logo">&copy; 2024 Yeokart. All rights reserved.</p>
-            </div>
 
-            <div class="footer-links">
-                <a href="#">Home</a>
-                <a href="customer_item.html">Shop</a>
-                <a href="#">Contact</a>
-                <a href="#">About Us</a>
-            </div>
-
-            <div class="social-links">
-                <li><a href="#" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
-                <li><a href="#" target="_blank"><i class="fab fa-twitter"></i></a></li>
-                <li><a href="#" target="_blank"><i class="fab fa-instagram"></i></a></li>
-            </div>
-        </div>
-    </footer>
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.0.7/dist/umd/popper.min.js"></script>
