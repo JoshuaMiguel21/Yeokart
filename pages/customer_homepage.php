ab44a4f828641dcf6b9fb62e9a848bfbdbf5bb86
JoshuaMiguel21 <joshuamiguel.embestro.cics@ust.edu.ph>
2024-02-28 21:36:21 +0800

    minor changes

--- pages/customer_homepage.php
+++ pages/customer_homepage.php
@@ -8,7 +8,7 @@
     <title>Yeokart</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
-    <link rel="stylesheet" href="/css/style_customer_homepage.css">
+    <link rel="stylesheet" href="/css/style_homepage_customer.css">
 </head>
 
 
