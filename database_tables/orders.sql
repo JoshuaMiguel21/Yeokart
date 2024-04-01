c0bb4e3591cf1e20d15dba0dcde174e2b3ffb545
IvanCastro58 <christianivan.castro.cics@ust.edu.ph>
2024-04-01 09:24:26 +0800

    Orders table

--- database_tables/orders.sql
+++ database_tables/orders.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 25, 2024 at 09:39 AM
+-- Generation Time: Apr 01, 2024 at 03:23 AM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -34,9 +34,20 @@ CREATE TABLE `orders` (
   `lastname` varchar(255) DEFAULT NULL,
   `address` text DEFAULT NULL,
   `items_ordered` text DEFAULT NULL,
+  `item_quantity` text DEFAULT NULL,
   `total` decimal(10,2) DEFAULT NULL,
-  `date_of_purchase` date DEFAULT NULL
+  `date_of_purchase` date DEFAULT NULL,
+  `status` enum('Pending','Processing','Shipped','Delivered') NOT NULL DEFAULT 'Pending'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`order_id`, `customer_id`, `firstname`, `lastname`, `address`, `items_ordered`, `item_quantity`, `total`, `date_of_purchase`, `status`) VALUES
+('66078e95dde1f', 2, 'Ivan', 'Castro', '54D Magsalin Compound Caingin Street Quezon City Metro Manila 1116', 'Naruto', '1', '600.00', '2024-03-30', 'Pending'),
+('6607902142cae', 2, 'Ivan', 'Castro', '54D Magsalin Compound Caingin Street Quezon City Metro Manila 1116', 'Zoro', '1', '500.00', '2024-03-30', 'Delivered'),
+('66079051e7f9b', 1, 'Ivan', 'Castro', 'Goodhaven Compound 2 Urbano Street Quezon City Metro Manila 1116', 'Naruto, Zoro', '6, 6', '1800.00', '2024-03-30', 'Pending');
 
 --
 -- Indexes for dumped tables
@@ -46,18 +57,7 @@ CREATE TABLE `orders` (
 -- Indexes for table `orders`
 --
 ALTER TABLE `orders`
-  ADD PRIMARY KEY (`order_id`),
-  ADD KEY `customer_id` (`customer_id`);
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `orders`
---
-ALTER TABLE `orders`
-  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `user_accounts` (`id`);
+  ADD PRIMARY KEY (`order_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
